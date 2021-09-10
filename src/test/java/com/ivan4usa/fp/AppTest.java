b03f144ecd8f9001062da57ac3c09aea4202804c
Ivan Poluboiarynov <ipoluboiarynov@madisoncollege.edu>
2021-09-10 12:53:02 -0500

    My firts commit

---
+++ src/test/java/com/ivan4usa/fp/AppTest.java
@@ -0,0 +1,20 @@
+package com.ivan4usa.fp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
