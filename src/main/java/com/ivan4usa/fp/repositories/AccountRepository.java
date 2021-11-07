62a925fc10f6fce64680337824b42e47a01b01f5
ivan4usa <isp_ua@yahoo.com>
2021-11-07 02:08:52 -0600

    canged query for postgres

--- src/main/java/com/ivan4usa/fp/repositories/AccountRepository.java
+++ src/main/java/com/ivan4usa/fp/repositories/AccountRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,10 +19,11 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findAccountById(@Param("id") Long id);
 
-    @Query(value = "SELECT ((CASE WHEN a.start_date <= :date THEN a.start_balance ELSE 0 END) + " +
-            "SUM(CASE WHEN r.record_type = 'INCOME' AND r.record_date <= :date THEN r.amount ELSE 0 END) - " +
-            "SUM(CASE WHEN r.record_type = 'EXPENSE' AND r.record_date <= :date THEN r.amount ELSE 0 END)) " +
-            "AS balance FROM fp_db.account AS a LEFT JOIN fp_db.record AS r ON a.id = r.account_id WHERE a.id=:id",
+    @Query(value = "SELECT ((CASE WHEN a.start_date <= cast(:date as date) THEN a.start_balance ELSE 0 END) + " +
+            "SUM(CASE WHEN r.record_type = 'INCOME' AND r.record_date <= cast(:date as date) THEN r.amount ELSE 0 END) - " +
+            "SUM(CASE WHEN r.record_type = 'EXPENSE' AND r.record_date <= cast(:date as date) THEN r.amount ELSE 0 END)) " +
+            "AS balance FROM fp_db.account AS a LEFT JOIN fp_db.record AS r ON a.id = r.account_id WHERE a.id=:id " +
+            "GROUP BY a.start_date, a.start_balance",
     nativeQuery = true)
     BigDecimal getBalanceByAccountId(
             @Param("id") Long id,
